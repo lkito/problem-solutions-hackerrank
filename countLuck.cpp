b15ee5b646d824f0c9550dbb11d4686b5846687b
lkito <45845329+lkito@users.noreply.github.com>
2018-12-29 14:48:57 +0400

    Create countLuck.cpp

---
+++ countLuck.cpp
@@ -0,0 +1,84 @@
+//https://www.hackerrank.com/challenges/count-luck/problem
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the countLuck function below.
+string countLuck(vector<string> matrix, int k) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nm_temp;
+        getline(cin, nm_temp);
+
+        vector<string> nm = split_string(nm_temp);
+
+        int n = stoi(nm[0]);
+
+        int m = stoi(nm[1]);
+
+        vector<string> matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            string matrix_item;
+            getline(cin, matrix_item);
+
+            matrix[i] = matrix_item;
+        }
+
+        int k;
+        cin >> k;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string result = countLuck(matrix, k);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
